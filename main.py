6526bb6eb40c7e074bae09372631248b3094e598
TwentyOneSpokes <martin140311@gmail.com>
2022-03-11 06:06:34 +0000

    Added an iterator to the roster list to populate using Controller objects. Transposed the final DataFrame to replicate populating columns first.

--- main.py
+++ main.py
@@ -10,6 +10,11 @@ L = Controller("Luke")
 
 roster = [[0 for i in range(8)] for j in range(10)]
 
+for i in range(1):
+    for j in range(len(roster[i])):
+        roster[i][j] = L.name
+
 roster_df = pd.DataFrame(roster)
+roster_df = roster_df.transpose()
     
 print(roster_df)
